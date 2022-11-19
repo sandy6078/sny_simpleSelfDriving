ca2f0ea413fafcc0d9f87a7b51fd7767a5ea425a
SANDY <yetas.dax@gmail.com>
2022-11-19 02:55:32 +0000

    update
    removed recipe remains

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,15 +8,9 @@ version '1.0.0'
 shared_scripts {
     'lang/*.json',
     'config.lua',
-    'shared/functions/sh_fn_main.lua'
 }
 
 client_scripts {
     'client/functions/cl_fn_main.lua',
     'client/cl_main.lua'
-}
-
-server_scripts {
-    'server/functions/sv_fn_main.lua',
-    'server/sv_main.lua'
 }
